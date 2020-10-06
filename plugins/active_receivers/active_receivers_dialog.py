53d48159a4b47a5f403b577eb47a02f40740817d
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2020-10-07 05:54:36 +0700

    add validation of dialog input

--- plugins/active_receivers/active_receivers_dialog.py
+++ plugins/active_receivers/active_receivers_dialog.py
@@ -24,8 +24,8 @@
 
 import os
 
-from qgis.PyQt import uic
-from qgis.PyQt import QtWidgets
+from qgis.PyQt import uic, QtWidgets, QtGui
+# from qgis.PyQt import QtWidgets, QtGui
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
@@ -36,9 +36,8 @@ class ActiveReceiversDialog(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
         super(ActiveReceiversDialog, self).__init__(parent)
-        # Set up the user interface from Designer through FORM_CLASS.
-        # After self.setupUi() you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+        float_validator = QtGui.QDoubleValidator()
+        self.lineEdit_azimuth.setValidator(float_validator)
+        self.lineEdit_inline.setValidator(float_validator)
+        self.lineEdit_crossline.setValidator(float_validator)
